f8fbe1e021ec694d2260b2ea8bcea6b4e0a0d82a
Dylan Murray <dymurray@redhat.com>
2019-03-08 15:18:01 -0500

    Get route restoration working

--- velero-plugins/route/restore.go
+++ velero-plugins/route/restore.go
@@ -17,16 +17,33 @@ limitations under the License.
 package route
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/heptio/velero/pkg/apis/velero/v1"
 	"github.com/heptio/velero/pkg/restore"
-	networkv1 "github.com/openshift/client-go/network/clientset/versioned/typed/network/v1"
+	"github.com/mitchellh/mapstructure"
+	routev1API "github.com/openshift/api/route/v1"
 	"github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/rest"
+	"strings"
 )
+
+type routingConfig struct {
+	Subdomain string `json:"subdomain"`
+}
+
+type imagePolicyConfig struct {
+	InternalRegistryHostname string `json:"internalRegistryHostname"`
+}
+
+type ApiServerConfig struct {
+	ImagePolicyConfig imagePolicyConfig `json:"imagePolicyConfig"`
+	RoutingConfig     routingConfig     `json:"routingConfig"`
+}
 
 // MyRestorePlugin is a restore item action plugin for Velero
 type RestorePlugin struct {
@@ -57,30 +74,50 @@ func (p *RestorePlugin) Execute(item runtime.Unstructured, restore *v1.Restore) 
 
 	metadata.SetAnnotations(annotations)
 
-	client, err := p.networkClient()
+	client, err := p.coreClient()
 	if err != nil {
+		p.Log.Info(fmt.Sprintf("ERROR: %v", err))
 		return nil, nil, err
 	}
-	subnets, err := client.HostSubnets().List(metav1.ListOptions{})
+	config, err := client.ConfigMaps("openshift-apiserver").Get("config", metav1.GetOptions{})
 	if err != nil {
+		p.Log.Info(fmt.Sprintf("ERROR: %v", err))
 		return nil, nil, err
 	}
-	clusternetworks, err := client.ClusterNetworks().List(metav1.ListOptions{})
+	serverConfig := ApiServerConfig{}
+	err = json.Unmarshal([]byte(config.Data["config.yaml"]), &serverConfig)
 	if err != nil {
+		p.Log.Info(fmt.Sprintf("ERROR: %v", err))
 		return nil, nil, err
 	}
-	p.Log.Info(fmt.Sprintf("subnet: %#v", subnets))
-	p.Log.Info(fmt.Sprintf("clusterNetworks: %#v", clusternetworks))
+
+	subdomain := serverConfig.RoutingConfig.Subdomain
+
+	route := routev1API.Route{}
+	obj := item.UnstructuredContent()
+	mapstructure.Decode(obj, &route)
+
+	host := route.Spec.Host
+	name := strings.Split(host, ".")[0]
+	newHost := name + "." + subdomain
+	route.Spec.Host = newHost
+	var out map[string]interface{}
+	objrec, _ := json.Marshal(route)
+	json.Unmarshal(objrec, &out)
+
+	item.SetUnstructuredContent(out)
+	p.Log.Info(fmt.Sprintf("New route: %v", newHost))
+	p.Log.Info(fmt.Sprintf("item: %#v", item))
 
 	return item, nil, nil
 }
 
-func (p *RestorePlugin) networkClient() (*networkv1.NetworkV1Client, error) {
+func (p *RestorePlugin) coreClient() (*corev1.CoreV1Client, error) {
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		return nil, err
 	}
-	client, err := networkv1.NewForConfig(config)
+	client, err := corev1.NewForConfig(config)
 	if err != nil {
 		return nil, err
 	}

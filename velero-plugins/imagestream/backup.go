13e931a61243b932ac2e70b982d5c5e1556ff929
Jason Montleon <jmontleo@redhat.com>
2019-04-22 14:22:52 -0400

    Disable imagestream copies when not running as part of a migration

--- velero-plugins/imagestream/backup.go
+++ velero-plugins/imagestream/backup.go
@@ -41,55 +41,57 @@ func (p *BackupPlugin) Execute(item runtime.Unstructured, backup *v1.Backup) (ru
 		annotations = make(map[string]string)
 	}
 
-	internalRegistry := annotations[common.BackupRegistryHostname]
-	if len(internalRegistry) == 0 {
-		return nil, nil, errors.New("backup cluster registry not found for annotation \"openshift.io/backup-registry-hostname\"")
-	}
-	migrationRegistry := backup.Annotations[common.MigrationRegistry]
-	if len(migrationRegistry) == 0 {
-		return nil, nil, errors.New("migration registry not found for annotation \"openshift.io/migration\"")
-	}
-	p.Log.Info(fmt.Sprintf("[is-backup] internal registry: %#v", internalRegistry))
-
-	localImageCopied := false
-	localImageCopiedByTag := false
-	for _, tag := range im.Status.Tags {
-		p.Log.Info(fmt.Sprintf("[is-backup] Backing up tag: %#v", tag.Tag))
-		specTag := findSpecTag(im.Spec.Tags, tag.Tag)
-		copyToTag := true
-		if specTag != nil && specTag.From != nil {
-			p.Log.Info(fmt.Sprintf("[is-backup] image tagged: %s, %s", specTag.From.Kind, specTag.From.Name))
-			// we have a tag.
-			copyToTag = false
+	if backup.Annotations[common.MigrateTypeAnnotation] != "" {
+		internalRegistry := annotations[common.BackupRegistryHostname]
+		if len(internalRegistry) == 0 {
+			return nil, nil, errors.New("backup cluster registry not found for annotation \"openshift.io/backup-registry-hostname\"")
 		}
-		// Iterate over items in reverse order so most recently tagged is copied last
-		for i := len(tag.Items) - 1; i >= 0; i-- {
-			dockerImageReference := tag.Items[i].DockerImageReference
-			if strings.HasPrefix(dockerImageReference, internalRegistry) {
-				localImageCopied = true
-				destTag := ""
-				if copyToTag {
-					localImageCopiedByTag = true
-					destTag = ":" + tag.Tag
-				}
-				srcPath := fmt.Sprintf("docker://%s", dockerImageReference)
-				destPath := fmt.Sprintf("docker://%s/%s/%s%s", migrationRegistry, im.Namespace, im.Name, destTag)
-				p.Log.Info(fmt.Sprintf("[is-backup] copying from: %s", srcPath))
-				p.Log.Info(fmt.Sprintf("[is-backup] copying to: %s", destPath))
+		migrationRegistry := backup.Annotations[common.MigrationRegistry]
+		if len(migrationRegistry) == 0 {
+			return nil, nil, errors.New("migration registry not found for annotation \"openshift.io/migration\"")
+		}
+		p.Log.Info(fmt.Sprintf("[is-backup] internal registry: %#v", internalRegistry))
 
-				manifest, err := copyImageBackup(srcPath, destPath)
-				if err != nil {
-					p.Log.Info(fmt.Sprintf("[is-backup] Error copying image: %v", err))
-					return nil, nil, err
+		localImageCopied := false
+		localImageCopiedByTag := false
+		for _, tag := range im.Status.Tags {
+			p.Log.Info(fmt.Sprintf("[is-backup] Backing up tag: %#v", tag.Tag))
+			specTag := findSpecTag(im.Spec.Tags, tag.Tag)
+			copyToTag := true
+			if specTag != nil && specTag.From != nil {
+				p.Log.Info(fmt.Sprintf("[is-backup] image tagged: %s, %s", specTag.From.Kind, specTag.From.Name))
+				// we have a tag.
+				copyToTag = false
+			}
+			// Iterate over items in reverse order so most recently tagged is copied last
+			for i := len(tag.Items) - 1; i >= 0; i-- {
+				dockerImageReference := tag.Items[i].DockerImageReference
+				if strings.HasPrefix(dockerImageReference, internalRegistry) {
+					localImageCopied = true
+					destTag := ""
+					if copyToTag {
+						localImageCopiedByTag = true
+						destTag = ":" + tag.Tag
+					}
+					srcPath := fmt.Sprintf("docker://%s", dockerImageReference)
+					destPath := fmt.Sprintf("docker://%s/%s/%s%s", migrationRegistry, im.Namespace, im.Name, destTag)
+					p.Log.Info(fmt.Sprintf("[is-backup] copying from: %s", srcPath))
+					p.Log.Info(fmt.Sprintf("[is-backup] copying to: %s", destPath))
+
+					manifest, err := copyImageBackup(srcPath, destPath)
+					if err != nil {
+						p.Log.Info(fmt.Sprintf("[is-backup] Error copying image: %v", err))
+						return nil, nil, err
+					}
+					p.Log.Info(fmt.Sprintf("[is-backup] manifest of copied image: %s", manifest))
 				}
-				p.Log.Info(fmt.Sprintf("[is-backup] manifest of copied image: %s", manifest))
 			}
 		}
-	}
-	p.Log.Info(fmt.Sprintf("copied at least one local image: %t", localImageCopied))
-	p.Log.Info(fmt.Sprintf("copied at least one local image by tag: %t", localImageCopiedByTag))
+		p.Log.Info(fmt.Sprintf("copied at least one local image: %t", localImageCopied))
+		p.Log.Info(fmt.Sprintf("copied at least one local image by tag: %t", localImageCopiedByTag))
 
-	im.Annotations = annotations
+		im.Annotations = annotations
+	}
 
 	var out map[string]interface{}
 	objrec, _ := json.Marshal(im)
